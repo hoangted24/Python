2f6e28fe65a4617a2454f489b32609e3b28b390a
Hoangted <hoangted24@gmail.com>
2015-08-24 12:12:40 +0700

    IF then Else

---
+++ score.py
@@ -0,0 +1,12 @@
+__author__ = 'HOANGTED'
+print("Hi")
+score = int(input('what is your score?'))
+if score >= 90:
+    print('grade is A')
+elif score >= 80:
+    print('grade is B')
+elif score >= 70:
+    print('grade is C')
+elif score >= 60:
+    print('grade is D')
+else: print('grade is F')
